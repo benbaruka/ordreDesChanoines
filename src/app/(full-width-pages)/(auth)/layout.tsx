21ba887d2cbd879bb80a6c25fcba6b3c2ac5d8a9
Ben Ickah <89651828+benbaruka@users.noreply.github.com>
2025-08-22 23:59:40 +0200

    Update layout.tsx

--- src/app/(full-width-pages)/(auth)/layout.tsx
+++ src/app/(full-width-pages)/(auth)/layout.tsx
@@ -17,9 +17,9 @@ export default function AuthLayout({
         <div className="relative flex h-screen w-full flex-col justify-center dark:bg-gray-900 sm:p-0 ">
           {children}
         
-          <div className="fixed bottom-6 right-6 z-50 hidden sm:block">
+{/*           <div className="fixed bottom-6 right-6 z-50 hidden sm:block">
             <ThemeTogglerTwo />
-          </div>
+          </div> */}
         </div>
       </ThemeProvider>
     </div>
